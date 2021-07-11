535cd4ba6feb4de8b03fc5ebafe3832b7fa999bb
jopemachine <jopemachine@naver.com>
2021-07-11 09:45:38 +0900

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+# arvis SVG Optimizer
+
+Let you optimize through [SVGO](https://github.com/svg/svgo) your copied SVG markup.
+
+![screenshot](assets/alfred-svgo.gif)
+
+## Settings
+
+You can define your own [SVGO configuration](https://github.com/svg/svgo#what-it-can-do).
---
+++ index.js
@@ -0,0 +1,53 @@
+const arvish = require('arvish');
+const SVGO = require('svgo');
+
+const options = [
+  'removeDoctype',
+  'removeXMLProcInst',
+  'removeComments',
+  'removeMetadata',
+  'removeXMLNS',
+  'removeEditorsNSData',
+  'cleanupAttrs',
+  'inlineStyles',
+  'minifyStyles',
+  'convertStyleToAttrs',
+  'cleanupIDs',
+  'removeRasterImages',
+  'removeUselessDefs',
+  'cleanupNumericValues',
+  'cleanupListOfValues',
+  'convertColors',
+  'removeUnknownsAndDefaults',
+  'removeNonInheritableGroupAttrs',
+  'removeUselessStrokeAndFill',
+  'removeViewBox',
+  'cleanupEnableBackground',
+  'removeHiddenElems',
+  'removeEmptyText',
+  'convertShapeToPath',
+  'moveElemsAttrsToGroup',
+  'moveGroupAttrsToElems',
+  'collapseGroups',
+  'convertPathData',
+  'convertTransform',
+  'removeEmptyAttrs',
+  'removeEmptyContainers',
+  'mergePaths',
+  'removeUnusedNS',
+  'sortAttrs',
+  'removeTitle',
+  'removeDesc',
+  'removeDimensions',
+  'removeStyleElement',
+  'removeScriptElement',
+];
+
+const plugins = options.map(option => ({
+  [option]: process.env[option] == 1 ? true : false,
+}));
+
+const svgo = new SVGO({ plugins });
+svgo.optimize(arvish.input, {}).then(res => {
+  console.log(res.data);
+});
